d5108bcf291e3579ff376ed5cb3c53d529bf6264
webdev-raj <codewithsapnap@gmail.com>
2025-04-27 17:43:03 +0530

    few things update

--- Day-21/question-2/script.js
+++ Day-21/question-2/script.js
@@ -7,36 +7,32 @@ let imageslink = [
     "https://images.unsplash.com/photo-1503431128871-cd250803fa41?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDgzfEpwZzZLaWRsLUhrfHxlbnwwfHx8fHw%3D",
     "https://images.unsplash.com/photo-1474511320723-9a56873867b5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDg5fEpwZzZLaWRsLUhrfHxlbnwwfHx8fHw%3D"
 ]
-console.log(imageslink.length)
 let currentIdx = 0
 let previous = document.querySelector("#previous")
 let next = document.querySelector("#next")
 let imagesPreviewer = document.querySelector(".imagesPreviewer")
 let newImage = document.createElement("img")
-next.addEventListener("click",()=>{
-    if(currentIdx==imageslink.length-1){
-        currentIdx=0
+next.addEventListener("click", () => {
+    if (currentIdx === imageslink.length - 1) {
+        currentIdx = 0
     }
-    else{
+    else {
         currentIdx++
     }
-    imagesLoad()
+    newImage.setAttribute("src", imageslink[currentIdx])
 })
-previous.addEventListener("click",()=>{
-    if(currentIdx==0){
-        currentIdx=imageslink.length-1
+previous.addEventListener("click", () => {
+    if (currentIdx === 0) {
+        currentIdx = imageslink.length - 1
     }
-    else{
+    else {
         currentIdx--
     }
-    imagesLoad()
+    newImage.setAttribute("src", imageslink[currentIdx])
 })
 
-function imagesLoad(){
-    newImage.classList.add("h-full","w-full","rounded-lg","bg-cover","bg-center")
-    newImage.setAttribute("src",imageslink[currentIdx])
-    imagesPreviewer.append(newImage)
-}
-imagesLoad()
+newImage.classList.add("h-full", "w-full", "rounded-lg", "bg-cover", "bg-center")
+newImage.setAttribute("src", imageslink[currentIdx])
+imagesPreviewer.append(newImage)
 
 
